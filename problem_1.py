fba3caae27b528751085f264b16eebdb6c0372f5
Nicolas Mechulam <nicomebu@gmail.com>
2019-04-28 12:18:55 +0100

    Problem 3

--- problem_1.py
+++ problem_1.py
@@ -2,15 +2,16 @@
 # The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
+import math
+
 def sum_cont(num):
-    return (num*(num + 1))/2
+    return (num * (num + 1)) / 2 
 
 def prob_1(first, second, mcm, limit):
-    sum_first  = sum_cont(limit / first) * first
-    sum_second = sum_cont(limit / second) * second
-    sum_mcm    = sum_cont(limit / mcm) * mcm
+    sum_first  = sum_cont(math.trunc(limit / first)) * first
+    sum_second = sum_cont(math.trunc(limit / second)) * second
+    sum_mcm    = sum_cont(math.trunc(limit / mcm)) * mcm
     return sum_first + sum_second - sum_mcm
 
-if __name__ == '__main__':
-    print(prob_1(3,5,15,1000))
+print(prob_1(3,5,15,1000))
 
---
+++ problem_3.py
@@ -0,0 +1,29 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+
+# What is the largest prime factor of the number 600851475143 ?
+
+
+def is_divisible(num, divisors):
+    for div in divisors:
+        if num % div == 0:
+            return True
+    return False
+
+def gen_prime():
+    prime_list = []
+    current = 2
+    while True:
+        prime_list.append(current)
+        yield current
+        while is_divisible(current, prime_list): current += 1 
+
+def prob_3(target):
+    prime_generator = gen_prime()
+    prime = None
+    while target != 1:
+        prime = next(prime_generator)
+        while target % prime == 0:
+            target /= prime
+    return prime      
+
+print(prob_3(600851475143))
